d5bfc4b4c30c7b0d812e5879db0f3007a9a28a9f
Sebastian <Sebastian>
2023-12-03 16:37:31 +0100

    Version 3

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,54 @@
 ﻿using System;
-
+int secureValue()
+{
+    int correctValue;
+    bool validInput = false;
+    do
+    {
+        string userInput = Console.ReadLine();
+        if (int.TryParse(userInput, out correctValue))
+        {
+            validInput = true;
+        }
+        else
+        {
+            Console.WriteLine("Wrong value, try again");
+        }
+    } while (!validInput);
+    return correctValue;
+}
+int displayResult(int startValue,int valueOne,int valueTwo)
+{
+    switch (startValue)
+    {
+        case 1:
+            displayValues(valueOne, valueTwo, startValue);
+            Console.WriteLine(addition(valueOne, valueTwo));
+            break;
+        case 2:
+            displayValues(valueOne, valueTwo, startValue);
+            Console.WriteLine(subtraction(valueOne, valueTwo));
+            break;
+        case 3:
+            displayValues(valueOne, valueTwo, startValue);
+            Console.WriteLine(multiplication(valueOne, valueTwo));
+            break;
+        case 4:
+            displayValues(valueOne, valueTwo, startValue);
+            Console.WriteLine(division(valueOne, valueTwo));
+            break;
+        case 5:
+            Console.WriteLine(exponentiation(valueOne, valueTwo));
+            break;
+        case 6:
+            Console.WriteLine(factorial(valueOne));
+            break;
+        default:
+            Console.WriteLine("wrong value, try again");
+            break;
+    }
+    return 0;
+}
 string checkSign (int sign)
     {
         if (sign == 1)
@@ -41,12 +90,10 @@ int multiplication(int a, int b)
 {
     return a * b;
 }
-
 double division(double a, double b)
 {
     return a / b;
 }
-
 int exponentiation(int a, int b)
 {
     int c = a;
@@ -81,57 +128,38 @@ do
 {
     startValue = 1;
     Console.WriteLine("Choose the option and press enter");
-    Console.WriteLine("1.Addition, 2.subtraction, 3.multiplication, 4.division, 5.exponentiation, 6.factorial 7. close the program");
-    startValue = Convert.ToInt32(Console.ReadLine());
-    if (startValue == 6)
+    Console.WriteLine("1.Addition, 2.subtraction, 3.multiplication, 4.division, 5.exponentiation, 6.factorial 7.close the program");
+    do
     {
-        Console.Write("enter value: ");
-        valueOne = Convert.ToInt32(Console.ReadLine());
-        valueTwo = 0;
-    }
-    else if (startValue == 7)
-    {
-        valueOne = 0;
-        valueTwo = 0;
-        Environment.Exit(0);
-    }
-    else
-    {
-        Console.Write("enter first value: ");
-        valueOne = Convert.ToInt32(Console.ReadLine());
-        Console.Write("enter second value: ");
-        valueTwo = Convert.ToInt32(Console.ReadLine());
-    }
- 
+        startValue = secureValue();
+        if (startValue == 6)
+        {
+            Console.Write("enter value: ");
+            valueOne = Convert.ToInt32(Console.ReadLine());
+            valueTwo = 0;
+        }
+        else if (startValue == 7)
+        {
+            valueOne = 0;
+            valueTwo = 0;
+            Environment.Exit(0);
+        }
+        else if (startValue < 6 && startValue > 0)
+        {
+            Console.Write("enter first value: ");
+            valueOne = secureValue();
+            Console.Write("enter second value: ");
+            valueTwo = secureValue();
+        }
+        else
+        {
+            valueOne = 0;
+            valueTwo = 0;
+            Console.WriteLine("Wrong value");
+        }
+    } while (startValue > 7);
     Console.Write("result: ");
-    switch (startValue)
-    {
-        case 1:
-            displayValues(valueOne, valueTwo, startValue);
-            Console.WriteLine(addition(valueOne, valueTwo));
-            break;
-        case 2:
-            displayValues(valueOne, valueTwo, startValue);
-            Console.WriteLine(subtraction(valueOne, valueTwo));
-            break;
-        case 3:
-            displayValues(valueOne, valueTwo, startValue);
-            Console.WriteLine(multiplication(valueOne, valueTwo));
-            break;
-        case 4:
-            displayValues(valueOne, valueTwo, startValue);
-            Console.WriteLine(division(valueOne, valueTwo));
-            break;
-        case 5:
-            Console.WriteLine(exponentiation(valueOne, valueTwo));
-            break;
-        case 6:
-            Console.WriteLine(factorial(valueOne));
-            break;
-        default:
-            Console.WriteLine("wrong value, try again");
-            break;
-    }
+    displayResult(startValue, valueOne, valueTwo);
     clearConsole();
     
-} while (startValue != 7);
+} while (true);
